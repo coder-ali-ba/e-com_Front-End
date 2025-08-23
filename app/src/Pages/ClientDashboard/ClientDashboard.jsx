1caa43685fd787d71abcda9dec61c10a932d61a2
coder-ali-ba <habibali8770@gmail.com>
2025-08-23 23:24:57 +0500

    changes

--- app/src/Pages/ClientDashboard/ClientDashboard.jsx
+++ app/src/Pages/ClientDashboard/ClientDashboard.jsx
@@ -8,18 +8,20 @@ import ClientDrawer from '../Components/ClientDrawer'
 function ClientDashboard() {
   return (
     <div>
-       <Stack width={"100%"} maxWidth={"1024px"} direction={"row"} justifyContent={"space-between"} margin={"auto"} bgcolor={"GrayText"} padding={"10px"}>
-        <Box>
-         <Typography variant="h4" color='white'>WelCome</Typography>
-        </Box>
-        <Box display={"flex"} gap={"10px"}>
+       <Box display={"flex"} sx={{width:"100%"}}  direction={"row"} margin={"auto"} justifyContent={"space-between"} bgcolor={"GrayText"} padding={"10px"}>
+         
+            <Typography variant="h4" color='white'>WelCome</Typography>
+         
+         
             <Button
               variant='contained'><Link to="/login" style={{textDecoration:"none", color:"white"}}>LogIn</Link>
             </Button>
+        
         </Box>
-      </Stack>
-      <h1>ClientDashboard</h1>
-      <ClientDrawer />
+      
+       <Stack width={"100%"}>
+        <ClientDrawer />
+       </Stack>
     </div>
   )
 }
--- app/src/Pages/Components/ClientDrawer.jsx
+++ app/src/Pages/Components/ClientDrawer.jsx
@@ -10,19 +10,15 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
+import { Stack } from '@mui/material';
 
 export default function ClientDrawer() {
-  const [open, setOpen] = React.useState(false);
-
-  const toggleDrawer = (newOpen) => () => {
-    setOpen(newOpen);
-  };
-
-  const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
+ 
+  return (    
+     <Box>
+      <List sx={{display:"flex"}}>
+        {['Store', 'Trending', 'Sales', ''].map((text, index) => (
+          <ListItem key={text} >
             <ListItemButton>
               <ListItemIcon>
                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
@@ -32,28 +28,6 @@ export default function ClientDrawer() {
           </ListItem>
         ))}
       </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
-
-  return (
-    <div>
-      <Button onClick={toggleDrawer(true)}>Open drawer</Button>
-      <Drawer open={open} onClose={toggleDrawer(false)}>
-        {DrawerList}
-      </Drawer>
-    </div>
+    </Box>       
   );
 }
